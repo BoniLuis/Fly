7e02e70758cd7b4b19e3b8cafa251a3b16fa99e8
Arthur Greco <arthurgreco9@gmail.com>
2023-03-24 22:07:29 -0300

    Rotas

---
+++ trabFinal/src/GerenciadorRotas.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+    public class GerenciadorRotas {
+
+        private ArrayList<Rotas> lista;
+
+        private GerenciadorRotas() {
+            lista = new ArrayList<>();
+        }
+
+        private static GerenciadorRotas instance;
+
+        public static GerenciadorRotas getInstance() {
+            if (instance == null)
+                instance = new GerenciadorRotas();
+            return instance;
+
+        }
+
+        public void inserir(Rotas rota) {
+            lista.add( rota );
+        }
+
+        public void inserir(int umCod, String umNome) {
+            lista.add( new Rotas(umCod, umNome) );
+        }
+
+        public Rotas pesquisar(int cod) {
+            for (Rotas rota: lista) {
+                if (Rotas.getCodigo() == cod)
+                    return rota;
+            }
+            return null;
+        }
+
+        public String toString() {
+            StringBuilder aux = new StringBuilder("\nCadastro de Voos\n- - - - - - - - - - -\n");
+            for (Rotas umRota : lista) {
+                aux.append( umRota.toString() + "\n");
+            }
+            return aux.toString();
+        }
+    }
+
+
+
