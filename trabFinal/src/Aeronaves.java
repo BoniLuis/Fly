328b40afb28548da5200e8a5b69dba7ac7a0ba6a
Leonardo Bertoletti <Leonardo.Bertoletti@edu.pucrs.br>
2023-04-12 02:33:54 -0300

    Corrigindo erro Gerenciador de aeronaves e aeronaves

--- trabFinal/src/Aeronaves.java
+++ trabFinal/src/Aeronaves.java
@@ -1,2 +1,17 @@
 package PACKAGE_NAME;public class Aeronaves {
+
+    private int capacidade;
+    private String nome,cod;
+public Aeronaves(String umCod, String umNome,int umaCapaci){
+    cod = umCod;
+    nome = umNome;
+    capacidade = umaCapaci;
+}
+
+public String getCodigo(){return cod;}
+
+public String toString(){
+    return "nome: " + nome + "  Capacidade: "+ capacidade+ "  Codigo: " + cod;
+}
+
 }
--- trabFinal/src/GerenciadorDeAeronaves.java
+++ trabFinal/src/GerenciadorDeAeronaves.java
@@ -1,2 +1,27 @@
 package PACKAGE_NAME;public class GerenciadorDeAeronaves {
+
+public class GerenciadorDeAeronaves {
+    private ArrayList<Aeronaves> Lista;
+    private StringBuilder rel;
+
+    public GerenciadorDeAeronaves(){Lista = new ArrayList<>();}
+
+    public void Inserir(Aeronaves aero){Lista.add(aero);}
+
+    public String ListarTodas(){
+        StringBuilder rel = new StringBuilder("Lista de Aeronaves"+"\n"+"------------------"+"\n");
+        for (Aeronaves listar: Lista) {
+            rel.append(listar.toString()+"\n");
+        }
+        return rel.toString();
+    }
+
+    public Aeronaves buscarPorCodigo(String umCod){
+        for (Aeronaves umaAero: Lista) {
+            if(umaAero.getCodigo()==umCod) return umaAero;
+        }
+            return null;
+
+    }
+
 }
